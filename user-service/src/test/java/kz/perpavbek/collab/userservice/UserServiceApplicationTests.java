905830fde14cf4872135c0c7c3184fd4d4845b84
perpavbek <pasha_perec@hotmail.com>
2026-03-28 15:36:52 +0500

    feat: user service initialized

---
+++ user-service/src/test/java/kz/perpavbek/collab/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.perpavbek.collab.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
