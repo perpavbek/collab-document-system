db64b328d02fc333823abbea1b685d3c18677a02
perpavbek <pasha_perec@hotmail.com>
2026-03-30 11:17:06 +0500

    feat: forget to add EditOperationRequest

--- version-control-service/src/main/java/kz/perpavbek/collab/versioncontrolservice/dto/request/EditOperationRequest.java
+++ version-control-service/src/main/java/kz/perpavbek/collab/versioncontrolservice/dto/request/EditOperationRequest.java
@@ -1,5 +1,6 @@
 package kz.perpavbek.collab.versioncontrolservice.dto.request;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import kz.perpavbek.collab.versioncontrolservice.enums.OperationType;
 import lombok.AllArgsConstructor;
@@ -13,7 +14,7 @@ import java.util.UUID;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class DocumentEditMessageRequest {
+public class EditOperationRequest {
 
     @NotBlank(message = "documentId is required")
     private UUID documentId;
@@ -21,11 +22,12 @@ public class DocumentEditMessageRequest {
     @NotBlank(message = "userId is required")
     private UUID userId;
 
-    @NotBlank(message = "position is required")
+    @Min(value = 0, message = "position must be >= 0")
     private int position;
 
-    @NotBlank(message = "text is required")
-    private String text;
+    private String content;
+
+    private Integer length;
 
     @NotBlank(message = "type is required")
     private OperationType type;
